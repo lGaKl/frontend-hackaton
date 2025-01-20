275514bf196e9e98818136eb79e53bb41de386f0
Henri Wattecamps <ririwatt13@gmail.com>
2025-01-20 15:12:16 +0100

    service budget fini

--- src/features/budget/services/commands/BudgetCreateCommand.ts
+++ src/features/budget/services/commands/BudgetCreateCommand.ts
@@ -1,3 +1,4 @@
 export interface BudgetCreateCommand {
     total : number;
+    date_budget : Date;
 }
--- src/features/budget/types/Budget.ts
+++ src/features/budget/types/Budget.ts
@@ -1,3 +1,5 @@
 export interface Budget {
-
+    budgetid?: number;
+    total : number;
+    date_budget : Date;
 }
