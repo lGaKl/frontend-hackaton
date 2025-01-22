a4b452d0eb636e20923fc55aa729998ddf3fc71e
anita <la228445@student.helha.be>
2025-01-22 23:05:50 +0100

    update budget service

--- src/features/budget/services/BudgetService.tsx
+++ src/features/budget/services/BudgetService.tsx
@@ -5,10 +5,11 @@ import {BudgetUpdateCommand} from "./commands/BudgetUpdateCommand.ts";
 const BUDGET_API_URL = import.meta.env.VITE_API_URL + "/budgets";
 
 export const postBudget: (budget: BudgetCreateCommand) => Promise<Budget> = async (budget: BudgetCreateCommand)=>{
+    console.log("Budget API URL:", BUDGET_API_URL);
     const response = await fetch (BUDGET_API_URL,{
         method: "POST",
         headers:{
-            "content-Type" : "application/json"
+            "Content-Type" : "application/json"
         },
         body: JSON.stringify(budget)
     });
@@ -26,11 +27,11 @@ export const deleteBudgets : (budgetId: number) => Promise<Response> = async (id
     });
 }
 
-export const updateBudgets : (id: number, budget: BudgetUpdateCommand) => Promise<Response> = async(id:number, budget: BudgetUpdateCommand )=>{
-    return await fetch (`${BUDGET_API_URL}/${id}`,{
-        method: "PATCH",
+export const updateBudgets : (budget: BudgetUpdateCommand) => Promise<Response> = async(budget: BudgetUpdateCommand )=>{
+    return await fetch (BUDGET_API_URL,{
+        method: "PUT",
         headers:{
-            "content-Type" : "application/json"
+            "Content-Type" : "application/json"
         },
         body:JSON.stringify(budget)
     });
