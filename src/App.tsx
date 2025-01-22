d54b4a2ebf7c7ac46b3abf0a49b8daa1b916d3ce
anita <la228445@student.helha.be>
2025-01-23 00:30:26 +0100

    temporary new budget form

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {LoginComponent} from "./core/components/login/LoginComponent.tsx";
 import {RegisterComponent} from "./core/components/register/RegisterComponent.tsx";
 import {AuthProvider} from "./features/auth/AuthContext.tsx";
 import 'react-toastify/dist/ReactToastify.css';
+import {NewBudgetComponent} from "./core/components/budget-page/NewBudgetComponent.tsx";
 
 function App() {
     return <>
@@ -20,6 +21,7 @@ function App() {
                     <Route path="/" element={<HomeComponent/>} />
                     {categoriesRoutes}
                     <Route path="/budget" element={<BudgetPageComponent/>} />
+                    <Route path="/budget/newBudget" element={<NewBudgetComponent/>}></Route>
                     <Route index element={<HomeComponent/>}></Route>
                     {transactionRoutes}
                     <Route path="/login" element={<LoginComponent/>} />
