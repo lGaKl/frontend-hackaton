9ff3a62a2fd233bbe65bb119b6246062987499a6
sabeohabe <sobosebastian@gmail.com>
2025-01-22 20:16:09 +0100

    add an animated SuspenseWrapper

--- src/shared/utils/SuspenseWrapper.tsx
+++ src/shared/utils/SuspenseWrapper.tsx
@@ -1,16 +1,36 @@
 import {lazy, Suspense} from "react";
-
+import loadingGIF from "../../assets/loading.gif"
 /*
 Ce fichier implémente un composant React qui utilise Suspense et lazy pour le chargement dynamique des composants.
  */
 interface Props {
     importFn: () => Promise<any>;
+
 }
 
-export function SuspenseWrapper({importFn}: Props) {
-    const LazyComponent = lazy(importFn);
+export function SuspenseWrapper({ importFn }: Props) {
+        const LazyComponent = lazy(importFn);
 
-    return <Suspense fallback="loading...">
-        <LazyComponent />
-    </Suspense>
-}
+    return (
+        <Suspense
+            fallback={
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100vh",
+                    }}
+                >
+                    <img
+                        src={loadingGIF}
+                        alt="Loading..."
+                        style={{ maxWidth: "300px", maxHeight: "300px" }}
+                    />
+                </div>
+            }
+        >
+            <LazyComponent />
+        </Suspense>
+    );
+    }
